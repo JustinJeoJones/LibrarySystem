329e91c88bacf1c4c64d7531462cf2f42f5fb10e
Michael Clark <clarkmic@protonmail.com>
2018-08-02 14:38:05 -0400

    Implementation of App class that handles inputs outputs for menu and getting user selections

---
+++ LibrarySystem/App.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.ComponentModel.Design;
+
+namespace LibrarySystem
+{
+    public class App
+    {
+        public static void ShowMenu() =>
+            Console.WriteLine(
+                "\n1-List catalog.\n2-Search by author\n3-Search by Title Keyword\n4-Return a Book\n5-Quit");
+
+        public static int GetMenuSelection()
+        {
+            do
+            {
+                Console.Write("Enter menu number: ");
+                var userInput = Console.ReadLine();
+                try
+                {
+                    int userInputAsInt = int.Parse(userInput);
+                    switch (userInputAsInt)
+                    {
+                        case 1:
+                        case 2:
+                        case 3:
+                        case 4:
+                        case 5:
+                            return userInputAsInt;
+                        default:
+                            Console.WriteLine("The selection have made isn't on the menu list");
+                            break;
+                    }
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Please enter a valid Menu Selection");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"{ex.GetType()} says {ex.Message}");
+                }
+
+                return GetMenuSelection();
+                if (!ContinueProgram())
+                {
+                    break;
+                }
+            } while (true);
+        }
+
+        public static bool ContinueProgram()
+        {
+            do
+            {
+                Console.WriteLine("Continue y/n");
+                var input = Console.ReadLine().ToLower();
+
+                if (input != "y" && input != "n")
+                {
+                    continue;
+                }
+
+                return input == "y";
+            } while (true);
+        }
+    }
+}
